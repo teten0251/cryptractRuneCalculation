0dd02aeae5b866add936d8dbbdc1508835f06bd2
RiKs33F <95114554+teten0251@users.noreply.github.com>
2021-12-08 23:37:50 +0900

    create form and script

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculation() {
+    var target = document.getElementById("status").value,
+        targetValue = document.getElementById("targetValue").value,
+        maxValue = document.getElementById("maxValue").value,
+        limitationValue = document.getElementById("limitationValue").value,
+        paRate = document.getElementById("paRate").value;
+
+    // calculation
+
+}
