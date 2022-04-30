2feac9b860627ae5a3591464d5f1cb98490a3bc7
RiKs33F <95114554+teten0251@users.noreply.github.com>
2022-04-30 20:10:06 +0900

    adding logic

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 "use strict";
 
-function calculate() {
-    // change color
-    // var rune = document.getElementById("rune-15");
-    // rune.className = "bg-danger m-2 p-3";
+const MAPPING_DIFF = {
+    "HP": 40,
+    "攻撃力": 20,
+    "防御力": 20,
+    "スピード": 10
+}
 
+const HP_RATE = {
+    "gold": 6,
+    "silver": 4,
+    "bronze": 2
+}
+
+const ATTACK_RATE = {
+    "gold": 5,
+    "silver": 3,
+    "bronze": 2
+}
+
+const DEFENSE_RATE = {
+    "gold": 6,
+    "silver": 4,
+    "bronze": 2
+}
+
+const SPEED_RATE = {
+    "gold": 4,
+    "silver": 3,
+    "bronze": 2
+}
+
+function calculate() {
     const status = document.getElementById("status").value,
         target = document.getElementById("target").value,
         lvmax = document.getElementById("lvmax").value,
@@ -18,8 +45,6 @@ function calculate() {
     if (!rate) {
         rate = 0;
     }
-
-    // calculation process
 
     let rune0 = document.getElementById("rune-0"),
         rune1 = document.getElementById("rune-1"),
@@ -95,7 +120,6 @@ function calculate() {
         rune33,
         rune34
     ];
-
     let newClassName = "";
     runes.forEach(rune => {
         newClassName = rune.className.replace("bg-danger", "bg-secondary");
@@ -112,5 +136,20 @@ function calculate() {
         return;
     }
 
+    const resultList = getCalculationList(status, Number(target), Number(lvmax), Number(max), Number(rate));
+    const n = 1;
+
 }
 
+function getCalculationList(status, target, lvmax, max, rate) {
+    let list = {};
+    let i = 0;
+    while (lvmax + MAPPING_DIFF[status] * i <= max) {
+        list[lvmax + MAPPING_DIFF[status] * i] = [];
+        i++;
+    }
+
+    // TODO
+
+    return [];
+}
