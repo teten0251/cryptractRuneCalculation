ca5110e330ab09884cba15858abb9c241b6c0707
RiKs33F <95114554+teten0251@users.noreply.github.com>
2022-04-30 21:17:11 +0900

    fixing view

--- script.js
+++ script.js
@@ -46,95 +46,26 @@ function calculate() {
         rate = 0;
     }
 
-    let rune0 = document.getElementById("rune-0"),
-        rune1 = document.getElementById("rune-1"),
-        rune2 = document.getElementById("rune-2"),
-        rune3 = document.getElementById("rune-3"),
-        rune4 = document.getElementById("rune-4"),
-        rune5 = document.getElementById("rune-5"),
-        rune6 = document.getElementById("rune-6"),
-        rune7 = document.getElementById("rune-7"),
-        rune8 = document.getElementById("rune-8"),
-        rune9 = document.getElementById("rune-9"),
-        rune10 = document.getElementById("rune-10"),
-        rune11 = document.getElementById("rune-11"),
-        rune12 = document.getElementById("rune-12"),
-        rune13 = document.getElementById("rune-13"),
-        rune14 = document.getElementById("rune-14"),
-        rune15 = document.getElementById("rune-15"),
-        rune16 = document.getElementById("rune-16"),
-        rune17 = document.getElementById("rune-17"),
-        rune18 = document.getElementById("rune-18"),
-        rune19 = document.getElementById("rune-19"),
-        rune20 = document.getElementById("rune-20"),
-        rune21 = document.getElementById("rune-21"),
-        rune22 = document.getElementById("rune-22"),
-        rune23 = document.getElementById("rune-23"),
-        rune24 = document.getElementById("rune-24"),
-        rune25 = document.getElementById("rune-25"),
-        rune26 = document.getElementById("rune-26"),
-        rune27 = document.getElementById("rune-27"),
-        rune28 = document.getElementById("rune-28"),
-        rune29 = document.getElementById("rune-29"),
-        rune30 = document.getElementById("rune-30"),
-        rune31 = document.getElementById("rune-31"),
-        rune32 = document.getElementById("rune-32"),
-        rune33 = document.getElementById("rune-33"),
-        rune34 = document.getElementById("rune-34");
-
     // init
-    let runes = [
-        rune0,
-        rune1,
-        rune2,
-        rune3,
-        rune4,
-        rune5,
-        rune6,
-        rune7,
-        rune8,
-        rune9,
-        rune10,
-        rune11,
-        rune12,
-        rune13,
-        rune14,
-        rune15,
-        rune16,
-        rune17,
-        rune18,
-        rune19,
-        rune20,
-        rune21,
-        rune22,
-        rune23,
-        rune24,
-        rune25,
-        rune26,
-        rune27,
-        rune28,
-        rune29,
-        rune30,
-        rune31,
-        rune32,
-        rune33,
-        rune34
-    ];
-    let newClassName = "";
-    runes.forEach(rune => {
-        newClassName = rune.className.replace("bg-danger", "bg-secondary");
-        rune.className = newClassName;
-    });
+    let tbody = document.getElementById("resultList");
+    tbody.innerHTML = "";
 
-    if (target < lvmax) {
-        newClassName = rune0.className.replace("bg-secondary", "bg-danger");
-        rune0.className = newClassName;
-        return;
-    } else if (target === lvmax) {
-        newClassName = rune1.className.replace("bg-secondary", "bg-danger");
-        rune1.className = newClassName;
-        return;
-    }
+    // let tr = document.createElement("tr");
+    // let td1 = document.createElement("td");
+    // let td2 = document.createElement("td");
+    // let td3 = document.createElement("td");
+    // tr.appendChild(td1);
+    // tr.appendChild(td2);
+    // tr.appendChild(td3);
+    // tbody.appendChild(tr);
+
+    // if (target < lvmax) {
+    //     td1.innerText = "調整不可";
+    //     return;
+    // } else if (target === lvmax) {
+    //     td1.innerText = "刻印なし";
+    //     return;
+    // }
 
     const resultList = getCalculationList(status, Number(target), Number(lvmax), Number(max), Number(rate));
     const n = 1;
