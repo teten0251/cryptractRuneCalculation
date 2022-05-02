8f00e3d0d077d23c98a3eec1c959ac7a14a5e979
RiKs33F <95114554+teten0251@users.noreply.github.com>
2022-05-02 17:39:36 +0900

    fix view

--- script.js
+++ script.js
@@ -31,6 +31,43 @@ const SPEED_RATE = {
     "bronze": 2
 }
 
+const MAPPING = {
+    0: ["0-0-0-0", "刻印なし"],
+    1: ["1-0-0-0", "銅刻印１つ"],
+    2: ["1-1-0-0", ""],
+    3: ["1-1-1-0", ""],
+    4: ["1-1-1-1", ""],
+    5: ["2-0-0-0", ""],
+    6: ["2-1-0-0", ""],
+    7: ["2-1-1-0", ""],
+    8: ["2-1-1-1", ""],
+    9: ["2-2-0-0", ""],
+    10: ["2-2-1-0", ""],
+    11: ["2-2-1-1", ""],
+    12: ["2-2-2-0", ""],
+    13: ["2-2-2-1", ""],
+    14: ["2-2-2-2", ""],
+    15: ["3-0-0-0", ""],
+    16: ["3-1-0-0", ""],
+    17: ["3-1-1-0", ""],
+    18: ["3-1-1-1", ""],
+    19: ["3-2-0-0", ""],
+    20: ["3-2-1-0", ""],
+    21: ["3-2-2-0", ""],
+    22: ["3-2-2-1", ""],
+    23: ["3-2-2-2", ""],
+    24: ["3-3-0-0", ""],
+    25: ["3-3-1-0", ""],
+    26: ["3-3-1-1", ""],
+    27: ["3-3-2-0", ""],
+    28: ["3-3-2-1", ""],
+    29: ["3-3-2-2", ""],
+    30: ["3-3-3-0", ""],
+    31: ["3-3-3-1", ""],
+    32: ["3-3-3-2", ""],
+    33: ["3-3-3-3", ""]
+}
+
 function calculate() {
     const status = document.getElementById("status").value,
         target = document.getElementById("target").value,
@@ -50,24 +87,33 @@ function calculate() {
     let tbody = document.getElementById("resultList");
     tbody.innerHTML = "";
 
-    // let tr = document.createElement("tr");
-    // let td1 = document.createElement("td");
-    // let td2 = document.createElement("td");
-    // let td3 = document.createElement("td");
-    // tr.appendChild(td1);
-    // tr.appendChild(td2);
-    // tr.appendChild(td3);
-    // tbody.appendChild(tr);
-
-    // if (target < lvmax) {
-    //     td1.innerText = "調整不可";
-    //     return;
-    // } else if (target === lvmax) {
-    //     td1.innerText = "刻印なし";
-    //     return;
-    // }
-
     const resultList = getCalculationList(status, Number(target), Number(lvmax), Number(max), Number(rate));
+    if (!resultList) {
+        let tr = document.createElement("tr");
+        let base = document.createElement("td");
+        let rune = document.createElement("td");
+        let description = document.createElement("td");
+        tr.appendChild(base);
+        tr.appendChild(rune);
+        tr.appendChild(td3);
+        tbody.appendChild(description);
+        base.innerText = "調整不可";
+    } else {
+        for (let n = 0; n < resultList.length; n++) {
+            let tr = document.createElement("tr");
+            let base = document.createElement("td");
+            let rune = document.createElement("td");
+            let description = document.createElement("td");
+            tr.appendChild(base);
+            tr.appendChild(rune);
+            tr.appendChild(description);
+            tbody.appendChild(tr);
+            base.innerText = resultList[n].base;
+            rune.innerText = resultList[n].rune;
+            description.innerText = resultList[n].description;
+        }
+    }
+
     const n = 1;
 
 }
@@ -80,7 +126,73 @@ function getCalculationList(status, target, lvmax, max, rate) {
         i++;
     }
 
-    // TODO
+    let runeRate = "";
+    switch (status) {
+        case "HP":
+            runeRate = HP_RATE;
+            break;
+        case "攻撃力":
+            runeRate = ATTACK_RATE;
+            break;
+        case "守備力":
+            runeRate = DEFENSE_RATE;
+            break;
+        case "スピード":
+            runeRate = SPEED_RATE;
+            break;
+    }
+    runeRate["gold"] = runeRate["gold"] / 100;
+    runeRate["silver"] = runeRate["silver"] / 100;
+    runeRate["bronze"] = runeRate["bronze"] / 100;
 
-    return [];
+    Object.keys(list).forEach(key => {
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 0 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 0 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 0 + runeRate["bronze"] * 2)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 0 + runeRate["bronze"] * 3)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 0 + runeRate["bronze"] * 4)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 1 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 1 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 1 + runeRate["bronze"] * 2)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 1 + runeRate["bronze"] * 3)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 2 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 2 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 2 + runeRate["bronze"] * 2)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 3 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 3 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 0 + runeRate["silver"] * 4 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 0 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 0 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 0 + runeRate["bronze"] * 2)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 0 + runeRate["bronze"] * 3)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 1 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 1 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 2 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 2 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 1 + runeRate["silver"] * 3 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 2 + runeRate["silver"] * 0 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 2 + runeRate["silver"] * 0 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 2 + runeRate["silver"] * 0 + runeRate["bronze"] * 2)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 2 + runeRate["silver"] * 1 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 2 + runeRate["silver"] * 1 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 2 + runeRate["silver"] * 2 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 3 + runeRate["silver"] * 0 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 3 + runeRate["silver"] * 0 + runeRate["bronze"] * 1)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 3 + runeRate["silver"] * 1 + runeRate["bronze"] * 0)));
+        list[key].push(Math.round(key * (1 + runeRate["gold"] * 4 + runeRate["silver"] * 0 + runeRate["bronze"] * 0)));
+    });
+
+    let result = [];
+    Object.keys(list).forEach(key => {
+        for (let i = 0; i <= list[key].length; i++) {
+            if (list[key][i] === target) {
+                let data = {};
+                data["base"] = key;
+                data["rune"] = MAPPING[i][0];
+                data["description"] = MAPPING[i][1];
+                result.push(data);
+            }
+        }
+    });
+    return result;
 }
