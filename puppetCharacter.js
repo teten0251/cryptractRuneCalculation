cda02b48706c4aeb48264a61d038ac6e96cdc9ae
RiKs33F <95114554+teten0251@users.noreply.github.com>
2022-05-03 00:02:55 +0900

    add data of puppet

--- puppetCharacter.js
+++ puppetCharacter.js
@@ -1,284 +1,272 @@
 window.PUPPET_CHARACTER = [
   {
     "id": "闇進化イロハ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2213,
+    "max": 3033,
     "rate": 0
   },
   {
     "id": "闇契約イロハ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2713,
+    "max": 3533,
     "rate": 0
   },
   {
     "id": "闇進化ヴァイパー",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2598,
+    "max": 3388,
     "rate": 0
   },
   {
     "id": "闇契約ヴァイパー",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2598,
+    "max": 3388,
     "rate": 0
   },
   {
     "id": "水進化ヴァイパー",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2598,
+    "max": 3338,
     "rate": 0
   },
   {
     "id": "水契約ヴァイパー",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2548,
+    "max": 3288,
     "rate": 0
   },
   {
     "id": "闇進化ヴァネイト",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2598,
+    "max": 3098,
     "rate": 0
   },
   {
     "id": "闇契約ヴァネイト",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2998,
+    "max": 3498,
     "rate": 0
   },
   {
     "id": "[解放]樹進化オペラ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2599,
+    "max": 3299,
+    "rate": 10
   },
   {
     "id": "[解放]樹契約オペラ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2599,
+    "max": 3299,
+    "rate": 10
   },
   {
     "id": "樹進化オペラ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2599,
+    "max": 3299,
     "rate": 0
   },
   {
     "id": "樹契約オペラ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2599,
+    "max": 3299,
     "rate": 0
   },
   {
     "id": "[解放]闇進化オペラ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
-  },
-  {
-    "id": "[解放]闇契約オペラ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2299,
+    "max": 2999,
+    "rate": 10
   },
   {
     "id": "闇進化オペラ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2299,
+    "max": 2999,
     "rate": 0
   },
   {
-    "id": "闇契約オペラ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
-  },
-  {
-    "id": "昇華グレーテル",
-    "lvmax": 0,
-    "max": 0,
+    "id": "火契約2グレーテル",
+    "lvmax": 2379,
+    "max": 3319,
     "rate": 0
   },
   {
     "id": "闇進化サロメ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2598,
+    "max": 3198,
     "rate": 0
   },
   {
     "id": "闇契約サロメ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2598,
+    "max": 3198,
     "rate": 0
   },
   {
     "id": "[解放]樹進化ジェペート",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2488,
+    "max": 2888,
+    "rate": 20
   },
   {
     "id": "[解放]樹契約ジェペート",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2488,
+    "max": 2888,
+    "rate": 20
   },
   {
     "id": "樹進化ジェペート",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2488,
+    "max": 2888,
     "rate": 0
   },
   {
     "id": "樹契約ジェペート",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2488,
+    "max": 2888,
     "rate": 0
   },
   {
     "id": "樹進化スイレン",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2596,
+    "max": 3006,
     "rate": 0
   },
   {
     "id": "樹契約スイレン",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2596,
+    "max": 3006,
     "rate": 0
   },
   {
     "id": "[解放]水進化スイレン",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2516,
+    "max": 3316,
+    "rate": 5
   },
   {
     "id": "[解放]水契約スイレン",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2306,
+    "max": 3106,
+    "rate": 10
   },
   {
     "id": "水進化スイレン",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2516,
+    "max": 3316,
     "rate": 0
   },
   {
     "id": "水契約スイレン",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2306,
+    "max": 3106,
     "rate": 0
   },
   {
     "id": "[解放]光進化セツナ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2340,
+    "max": 3140,
+    "rate": 5
   },
   {
     "id": "[解放]光契約セツナ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2550,
+    "max": 3350,
+    "rate": 5
   },
   {
     "id": "光進化セツナ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2340,
+    "max": 3140,
     "rate": 0
   },
   {
     "id": "光契約セツナ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2550,
+    "max": 3350,
     "rate": 0
   },
   {
     "id": "闇進化ツァトーレ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2399,
+    "max": 2839,
     "rate": 0
   },
   {
     "id": "闇契約ツァトーレ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2599,
+    "max": 3039,
     "rate": 0
   },
   {
     "id": "[解放]水進化トワネロ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 1572,
+    "max": 2022,
+    "rate": 30
   },
   {
     "id": "水進化トワネロ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 1572,
+    "max": 2022,
     "rate": 0
   },
   {
     "id": "[解放]火進化ナユタ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2627,
+    "max": 3447,
+    "rate": 10
   },
   {
     "id": "[解放]火契約ナユタ",
-    "lvmax": 0,
-    "max": 0,
-    "rate": 0
+    "lvmax": 2627,
+    "max": 3447,
+    "rate": 10
   },
   {
     "id": "火契約2ナユタ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2580,
+    "max": 3400,
     "rate": 0
   },
   {
     "id": "火進化ナユタ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2627,
+    "max": 3447,
     "rate": 0
   },
   {
     "id": "火契約ナユタ",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2627,
+    "max": 3447,
     "rate": 0
   },
   {
     "id": "光進化ハーメルン",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2425,
+    "max": 3155,
     "rate": 0
   },
   {
     "id": "光契約ハーメルン",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2085,
+    "max": 2815,
     "rate": 0
   },
   {
     "id": "光進化パシタラネア",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 1722,
+    "max": 2022,
     "rate": 0
   },
   {
     "id": "光契約パシタラネア",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 1722,
+    "max": 2022,
     "rate": 0
   },
   {
     "id": "闇契約ハトホル",
-    "lvmax": 0,
-    "max": 0,
+    "lvmax": 2394,
+    "max": 3304,
     "rate": 0
   }
 ]
